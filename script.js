71481173f84032ff28358ece1a0101fbffccc813
Shubham Sights <106985788+ShubhamGuha1007@users.noreply.github.com>
2024-07-12 08:05:24 +0530

    Resume Website

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+function showAlert() {
+    alert('Phone Number - 6290814281');
+    }  
